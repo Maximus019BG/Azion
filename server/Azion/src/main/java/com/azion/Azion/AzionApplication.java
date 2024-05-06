5e8998120d09ff90a17bdb31321d215031d51b0e
Maximus019BG <maxralev@abv.bg>
2024-05-06 14:26:32 +0300

    Add(backend):basic setup for springboot app

---
+++ server/Azion/src/main/java/com/azion/Azion/AzionApplication.java
@@ -0,0 +1,14 @@
+package com.azion.Azion;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzionApplication.class, args);
+	}
+
+}
