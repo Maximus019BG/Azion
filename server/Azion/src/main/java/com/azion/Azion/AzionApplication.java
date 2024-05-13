16bd2a9ab44980bb3eeaffccd118a1136ee2406c
Maximus019BG <maxralev@abv.bg>
2024-05-14 01:43:54 +0300

    Feat(backend):Added working sign up with email validation and unique ID created local db and tested every user has name age email password role

--- server/Azion/src/main/java/com/azion/Azion/AzionApplication.java
+++ server/Azion/src/main/java/com/azion/Azion/AzionApplication.java
@@ -3,11 +3,16 @@ package com.azion.Azion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AzionApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(AzionApplication.class, args);
 	}
 
---
+++ server/Azion/src/main/java/com/azion/Azion/User/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.azion.Azion.User.Repository;
+
+import com.azion.Azion.User.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+}
