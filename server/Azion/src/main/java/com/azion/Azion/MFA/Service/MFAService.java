259f5224825613a72da6b9d85c6003e4c387a491
Maximus019BG <maxralev@abv.bg>
2024-05-13 11:38:52 +0300

    Add(backend):made MFA and User folders

---
+++ server/Azion/src/main/java/com/azion/Azion/MFA/Service/MFAService.java
@@ -0,0 +1,7 @@
+package com.azion.Azion.MFA.Service;
+
+public class MFAService {
+    public static String mfa() {
+        return "<h1>mfaaaaaaaaaa</h1>";
+    }
+}
---
+++ server/Azion/src/main/java/com/azion/Azion/User/Model/User.java
@@ -0,0 +1,5 @@
+package com.azion.Azion.User.Model;
+
+public class User {
+    //TODO: Implement User Model
+}
