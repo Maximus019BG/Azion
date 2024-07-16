fc9e2c701f34f8859f01c385b060a6a63e5a78a6
SimoSabev <sabevsimeon08@gmail.com>
2024-07-16 18:39:26 +0300

    fix(landing)

--- client/azion-f/app/layout.tsx
+++ client/azion-f/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href={logo.src} />
       </head>
-      <body className={` ${inter.className} bg-[#f7f3e8]`}>
+      <body className={` ${inter.className} bg-background text-foreground overflow-x-hidden  `}>
         {children}
       </body>
     </html>
--- client/azion-f/app/page.tsx
+++ client/azion-f/app/page.tsx
@@ -1,7 +1,5 @@
-"use client"
+"use client";
 import { motion } from "framer-motion";
-import Image from "next/image";
-import background from "../public/Image.png";
 import Navbar from "./components/Navbar";
 import { Commissioner, Poppins } from "next/font/google";
 
@@ -10,48 +8,47 @@ const HeaderText = Poppins({ subsets: ["latin"], weight: "600" });
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen bg-[#f7f3e8] overflow-x-hidden">
+    <div className="h-[500vh] w-full overflow-x-hidden">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        className="flex flex-col justify-center items-center lg:items-start px-6 lg:px-56 py-10 lg:py-28"
+        className="flex flex-col justify-center items-center lg:items-start px-6 lg:px-56 py-10 lg:py-28 w-full"
       >
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.3 }}
-          className="max-w-2xl mt-10 gap-8 text-center lg:text-left"
+          className="max-w-2xl mt-10 gap-8 text-center lg:text-left w-full"
         >
-          <h1 className={`text-[#18b7be] text-3xl md:text-4xl lg:text-6xl ${HeaderText.className}`}>
+          <h1 className={`text-3xl gradient-text md:text-4xl lg:text-6xl ${HeaderText.className}`}>
             Improve your workflow and secure your company with{" "}
-            <span className={`text-[#072a40] text-4xl md:text-6xl lg:text-8xl ${azionText.className}`}>
+          </h1>
+          <span className={` neon-text text-4xl md:text-6xl lg:text-8xl ${azionText.className}`}>
               Azion
             </span>
-          </h1>
           <motion.div
             initial={{ opacity: 0, x: -20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6, delay: 0.6 }}
-            className="flex flex-col sm:flex-row justify-center items-center gap-4 md:gap-8 mt-8 md:mt-12 lg:mt-16"
+            className="flex flex-col sm:flex-row justify-center items-center gap-4 md:gap-8 mt-8 md:mt-12 lg:mt-16 w-full"
           >
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className={`w-40 md:w-64 lg:w-80 h-10 md:h-12 lg:h-14 bg-[#18b7be] text-slate-200 rounded-2xl text-base md:text-lg lg:text-xl hover:bg-[#139299] ${HeaderText.className}`}
+              className={`w-40 md:w-64 lg:w-80 h-10 md:h-12 lg:h-14 bg-[#18b7be] rounded-2xl text-base md:text-lg lg:text-xl hover:bg-[#139299] ${HeaderText.className}`}
             >
               Register
             </motion.button>
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className={`w-40 md:w-64 lg:w-80 h-10 md:h-12 lg:h-14 bg-[#072a40] text-slate-200 rounded-2xl text-base md:text-lg lg:text-xl hover:bg-[#106092] ${HeaderText.className}`}
+              className={`w-40 md:w-64 lg:w-80 h-10 md:h-12 lg:h-14 bg-[#072a40] rounded-2xl text-base md:text-lg lg:text-xl hover:bg-[#106092] ${HeaderText.className}`}
             >
               Continue
             </motion.button>
           </motion.div>
         </motion.div>
-
         <Navbar />
       </motion.div>
     </div>
