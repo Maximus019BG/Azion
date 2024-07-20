785f5d820add5f4160021e30b584c403b29bee11
Maximus019BG <maxralev@abv.bg>
2024-07-20 16:30:53 +0300

    add:docker docker-compose.yml and dockerfile to server

--- server/Azion/src/test/java/com/example/Azion/AzionApplicationTests.java
+++ server/Azion/src/test/java/com/example/Azion/AzionApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.Azion;
 
+import com.azion.Azion.AzionApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = AzionApplication.class)
 class AzionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    static Dotenv dotenv = Dotenv.configure()
+                                  .directory("/home/max/IdeaProjects/Azion/")
+                                  .load();
 
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> dotenv.get("DB_URL"));
+        registry.add("spring.datasource.username", () -> dotenv.get("DB_USERNAME"));
+        registry.add("spring.datasource.password", () -> dotenv.get("DB_PASSWORD"));
+        registry.add("secretJWT", () -> dotenv.get("SECRET_JWT"));
+        registry.add("issuerName", () -> dotenv.get("ISSUER_NAME"));
+        registry.add("secretMFA", () -> dotenv.get("SECRET_MFA"));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
