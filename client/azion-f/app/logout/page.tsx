5ab11fda8fffec8168677d35abd2299d1191b8cd
Maximus019BG <maxralev@abv.bg>
2024-08-25 12:41:16 +0300

    chore(server):log out from session fix cookies are now removed

--- client/azion-f/app/logout/page.tsx
+++ client/azion-f/app/logout/page.tsx
@@ -97,105 +97,10 @@ const SessionCheck = () => {
 };
 
 const LogOut = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [age, setAge] = useState("");
-    const [password, setPassword] = useState("");
-    const [password2, setPassword2] = useState("");
-    const [isOrgOwner, setIsOrgOwner] = useState(false);
-    const [role, setRole] = useState("");
-    const [step, setStep] = useState(0);
 
     useEffect(() => {
         SessionCheck();
     }, []);
-
-    const handleSubmit = () => {
-        if (isOrgOwner) {
-            setRole("owner");
-        } else if (!isOrgOwner) {
-            setRole("none");
-        }
-        const userData = {
-            name,
-            email,
-            age,
-            password,
-            role,
-            mfaEnabled: true,
-        };
-        if (password === password2) {
-            AxiosFunction(userData, isOrgOwner);
-        } else {
-            alert("Passwords do not match.");
-        }
-    };
-
-    const handleNextStep = () => {
-        if (step === 3) {
-            // Skip step 4
-            setStep(step + 2);
-        } else if (step < inputFields.length - 1) {
-            setStep(step + 1);
-        }
-    };
-
-    const inputFields: InputField<any>[] = [
-        {
-            label: "Enter your username:",
-            value: name,
-            onChange: setName,
-            type: "text",
-        },
-        {
-            label: "Enter your email:",
-            value: email,
-            onChange: setEmail,
-            type: "email",
-        },
-        { label: "Enter your age:", value: age, onChange: setAge, type: "date" },
-        {
-            label: "Password:",
-            value: password,
-            onChange: setPassword,
-            type: "password",
-            combinedWith: "Confirm Password:",
-        },
-        {
-            label: "Confirm Password:",
-            value: password2,
-            onChange: setPassword2,
-            type: "password",
-            combinedWith: "Password:",
-        },
-        {
-            label: "I'm an organization owner",
-            value: isOrgOwner,
-            onChange: setIsOrgOwner,
-            type: "checkbox",
-        },
-    ];
-
-    const getCurrentFields = () => {
-        if (step === 3) {
-            // Group fields
-            return inputFields.filter(
-                (field) =>
-                    field.label === "Password:" || field.label === "Confirm Password:"
-            );
-        }
-
-        // оther steps
-        return [inputFields[step]];
-    };
-
-    const stepLabels = [
-        "Register",
-        "Enter Details",
-        "Password",
-        "Organization?",
-        "Submit",
-    ];
 
     return (
         <>
