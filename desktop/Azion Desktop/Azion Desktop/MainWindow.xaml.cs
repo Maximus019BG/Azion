d030762ab9fab9993aa28a5569baf72add92f31c
Maximus019BG <maxralev@abv.bg>
2024-09-23 20:03:27 +0300

    feat(desktop):desktop app made in cs

---
+++ desktop/Azion Desktop/Azion Desktop/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows;
+
+namespace Azion_Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            await webViewControl.EnsureCoreWebView2Async(null);
+            /*For tests use http://localhost:3000/login
+            Don't forget to Change it
+            TODO:Change it*/
+            webViewControl.Source = new Uri("https://ev-f.vercel.app");
+         
+        }
+    }
+}
--- desktop/manage.h
+++
@@ -1,6 +0,0 @@
-#ifndef MANAGE_H
-#define MANAGE_H
-
-void ensureAppIcon();
-
-#endif
