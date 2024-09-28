af9f02dee180a81387c9cdf91ed6192128653f39
Maximus019BG <maxralev@abv.bg>
2024-09-28 21:00:31 +0300

    feat(chat):end-to-end encryption stage 1 basic encryption with AES algorithm

--- server/Azion/src/main/java/com/azion/Azion/Chat/Controller/ChatController.java
+++ server/Azion/src/main/java/com/azion/Azion/Chat/Controller/ChatController.java
@@ -23,7 +23,6 @@ public class ChatController {
 
     @MessageMapping("/privateMessage")
     public void sendPrivateMessage(Message message) {
-        System.out.println(message.getContent());
         messagingTemplate.convertAndSendToUser(message.getTo(), "/private", message);
     }
 }
