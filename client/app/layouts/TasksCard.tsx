81a52750ee7ddd3697a99873b13e7a4b9c27cad1
SimoSabev <sabevsimeon08@gmail.com>
2024-10-10 10:12:07 +0300

    style(task cards icons info)

--- client/app/layouts/TasksCard.tsx
+++ client/app/layouts/TasksCard.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import {FaBuilding,} from "react-icons/fa";
+import {FaBuilding} from "react-icons/fa";
 import {TbFileDescription} from "react-icons/tb";
 import {GrStatusWarning} from "react-icons/gr";
 import {IoTimer} from "react-icons/io5";
 import {MdOutlineDriveFileRenameOutline} from "react-icons/md";
-
 
 interface Task {
     title: string;
@@ -50,32 +49,52 @@ const TasksCard: React.FC<Task> = ({
             <div className="absolute top-4 right-4 flex items-center group">
                 {getPriorityIcon(priority)}
                 <div
-                    className="absolute top-0 right-0 mt-6 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
+                    className="absolute top-0 left-0 mt-6 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
                     Priority: {priority.toUpperCase()}
                 </div>
             </div>
             <div className="min-w-96 p-2">
-                <div
-                    className="font-bold text-xl mb-4 flex items-center text-white">
-                    <FaBuilding className="mr-2 text-blue-500"/> {title}
+                <div className="font-bold text-xl mb-4 flex items-center text-white group">
+                    <FaBuilding className="mr-2 text-blue-500 cursor-pointer"/>
+                    {title}
+                    <div
+                        className="absolute top-2 left-2 mt-0 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
+                        Title: {title}
+                    </div>
                 </div>
-                <p className="text-gray-200 text-base flex items-center mb-2">
-                    <TbFileDescription className="mr-2 text-2xl text-red-500"/> {description}
+                <p className="text-gray-200 text-base flex items-center mb-2 group">
+                    <TbFileDescription className="mr-2 text-2xl text-red-500"/>
+                    {description}
+                    <div
+                        className="absolute top-12 left-0 mt-0 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
+                        Description: {description}
+                    </div>
                 </p>
-                <p className="text-gray-200 text-base flex items-center mb-2">
-                    <GrStatusWarning className="mr-2 text-2xl text-green-500"/> {status}
+                <p className="text-gray-200 text-base flex items-center mb-2 group">
+                    <GrStatusWarning className="mr-2 text-2xl text-green-500"/>
+                    {status}
+                    <div
+                        className="absolute top-16 left-0 mt-6 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
+                        Status: {status}
+                    </div>
                 </p>
-                <p className="text-gray-200 text-base flex items-center mb-2">
-                    <IoTimer className="mr-2 text-2xl text-yellow-500"/> {data}
+                <p className="text-gray-200 text-base flex items-center mb-2 group">
+                    <IoTimer className="mr-2 text-2xl text-yellow-500"/>
+                    {data}
+                    <div
+                        className="absolute top-28 left-0 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
+                        Date: {data}
+                    </div>
                 </p>
-                <p className="text-gray-200 text-base flex items-center">
-                    <MdOutlineDriveFileRenameOutline className="mr-2 text-2xl text-purple-500"/> {createdBy}
+                <p className="text-gray-200 text-base flex items-center group">
+                    <MdOutlineDriveFileRenameOutline className="mr-2 text-2xl text-purple-500"/>
+                    {createdBy}
                     {isCreator && (
                         <div className="relative inline-flex items-center group">
                             <span className="text-xs">&nbsp;(you)</span>
                             <div
-                                className="absolute hidden group-hover:block left-full top-1/2 transform -translate-y-1/2 ml-2 bg-gray-800 text-white text-xs rounded-lg py-2 px-4 shadow-lg z-10 max-w-xs">
-                                You are the creator of this task
+                                className="absolute -bottom-1 left-10 mt-6 hidden group-hover:block bg-gray-700 text-white text-xs rounded py-1 px-2">
+                                Creator
                             </div>
                         </div>
                     )}
