556e4ffe9e6c776ce5d4d75a53efd6e2f4b38903
Maximus019BG <maxralev@abv.bg>
2024-10-15 21:39:20 +0300

    fix:prod bugs

--- client/app/api/config.ts
+++ client/app/api/config.ts
@@ -1,2 +1,2 @@
-export const apiUrl: string = "https://api.azion.online/api"
-export const chatUrl: string = "https://api.azion.online/chat" //!http for the handshake => upgrade to ws
+export const apiUrl: string = "https://api.azion.online:8443/api"
+export const chatUrl: string = "https://api.azion.online:8443/chat" //!http for the handshake => upgrade to ws
--- server/Azion/src/main/java/com/azion/Azion/Config/WebConfig.java
+++ server/Azion/src/main/java/com/azion/Azion/Config/WebConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
