47019d4e798fd3d736b85347719f5c1a8835458e
SimoSabev <sabevsimeon08@gmail.com>
2024-11-24 16:45:08 +0200

    style: account cards

--- client/app/(org)/dashboard/[org]/settings/employees/page.tsx
+++ client/app/(org)/dashboard/[org]/settings/employees/page.tsx
@@ -3,13 +3,11 @@ import List from '@/app/components/_employees/list';
 import React from 'react'
 
 const Employees = () => {
-  return (
-    <div className='bg-background w-full h-screen'>
-      <div>
-        <List />
-      </div>
-    </div>
-  )
+    return (
+        <div className='bg-background w-full h-screen'>
+            <List/>
+        </div>
+    )
 }
 
 export default Employees;
