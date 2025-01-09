bfe87f8df81518144bd9f6d6ba4c275edc526caf
Maximus019BG <maxralev019@gmail.com>
2025-01-10 01:34:10 +0200

    fix(client): build bug window undefined

--- client/app/(org)/dashboard/[org]/settings/employees/page.tsx
+++ client/app/(org)/dashboard/[org]/settings/employees/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 import List from '@/app/components/_employees/list';
-import React from 'react'
+import React, {useEffect} from 'react'
+import {UserData} from "@/app/func/funcs";
 
 const Employees = () => {
+    useEffect(()=>{
+
+        UserData().then((response)=>{
+           if(response.roleLevel < 0 || response.roleLevel > 2 ){
+               window.location.href = "/organizations";
+           }
+        })
+    })
+
     return (
         <div className='bg-background w-full h-screen'>
             <List/>
