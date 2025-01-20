3416855578e7328d13b68526ac762b753f50172b
Maximus019BG <maxralev019@gmail.com>
2025-01-20 23:09:05 +0200

    chore: config.ts urls

--- client/app/api/config.ts
+++ client/app/api/config.ts
@@ -1,5 +1,5 @@
-export const apiUrl: string = "https://api.azion.online/api" //http://localhost:8080/api  https://api.azion.online/api
-export const chatUrl: string = "https://api.azion.online/chat" //!https for the handshake => upgrade to ws http://localhost:8080/chat     https://api.azion.online/chat
+export const apiUrl: string = "http://localhost:8080/api" //  https://api.azion.online/api
+export const chatUrl: string = "http://localhost:8080/chat" //!https for the handshake => upgrade to ws     https://api.azion.online/chat
 
 const getClientUrl = () => {
     if (typeof window !== "undefined" && window.location) {
