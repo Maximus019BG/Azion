53312d033da5cd69e4552d3281281eda0b8cc156
Maximus019BG <maxralev019@gmail.com>
2025-03-01 18:13:35 +0200

    test: prod test 3

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,67 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    darkMode: ["class"],
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "*.{js,ts,jsx,tsx,mdx}",
-        "app/**/*.{ts,tsx}",
-        "components/**/*.{ts,tsx}",
     ],
     theme: {
         extend: {
             colors: {
-                border: "hsl(var(--border))",
-                input: "hsl(var(--input))",
-                ring: "hsl(var(--ring))",
-                background: "rgb(var(--background))",
-                foreground: "rgb(var(--foreground))",
-                primary: {
-                    DEFAULT: "hsl(var(--primary))",
-                    foreground: "hsl(var(--primary-foreground))",
-                },
-                secondary: {
-                    DEFAULT: "hsl(var(--secondary))",
-                    foreground: "hsl(var(--secondary-foreground))",
-                },
-                destructive: {
-                    DEFAULT: "hsl(var(--destructive))",
-                    foreground: "hsl(var(--destructive-foreground))",
-                },
-                muted: {
-                    DEFAULT: "hsl(var(--muted))",
-                    foreground: "hsl(var(--muted-foreground))",
-                },
-                popover: {
-                    DEFAULT: "hsl(var(--popover))",
-                    foreground: "hsl(var(--popover-foreground))",
-                },
-                card: {
-                    DEFAULT: "hsl(var(--card))",
-                    foreground: "hsl(var(--card-foreground))",
-                },
-                lightAccent: "rgb(var(--lightAccent))",
-                accent: "rgb(var(--accent))",
-                neonAccent: "rgb(var(--neonAccent))",
-            },
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
+                background: 'rgb(var(--background))',
+                foreground: 'rgb(var(--foreground))',
+                muted: 'rgb(var(--muted))',
+                accent: 'rgb(var(--accent))',
+                lightAccent: 'rgb(var(--lightAccent))',
+                neonAccent: 'rgb(var(--neonAccent))',
+
             },
         },
     },
-    plugins: [require("daisyui"), require("tailwindcss-animate")],
+    plugins: [require('daisyui'),],
     daisyui: {
         themes: [
             {
                 dark: {
                     ...require("daisyui/src/theming/themes")["dark"],
                     accent: "#0ea5e9",
-                },
-            },
-        ],
+                }
+            }
+        ]
     },
-}
+};
