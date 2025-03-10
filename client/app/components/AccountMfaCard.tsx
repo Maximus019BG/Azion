0b4d35239c055ae38dd0c9804d8261fb34fdf225
Maximus019BG <maxralev019@gmail.com>
2025-03-11 00:21:30 +0200

    v0.7.2

--- client/app/components/AccountMfaCard.tsx
+++ client/app/components/AccountMfaCard.tsx
@@ -128,7 +128,7 @@ const AccountMfaCard = () => {
             )}
 
             {/* Confirm Modal (For non-MFA users) */}
-            {showConfirmModal && !isMfaEnabled && (
+            {showConfirmModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                     <div className="bg-gray-900 p-6 rounded-lg shadow-lg max-w-md w-full">
                         <h2 className="text-xl font-bold mb-4 text-gray-200">Confirm Disabling FaceID</h2>
