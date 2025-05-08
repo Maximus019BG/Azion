1a5a9066413c6874d664f8ee2a36a9adcb2b94b2
Maximus019BG <maxralev019@gmail.com>
2025-05-08 21:04:53 +0300

    refactor(fix): Bug with forgot password

--- client/app/reset-password/page.tsx
+++ client/app/reset-password/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import {useSearchParams} from "next/navigation";
+import React, {Suspense, useState} from "react";
 import axios from "axios";
-import { apiUrl } from "@/app/api/config";
-import { Poppins } from "next/font/google";
-import { Suspense } from 'react';
+import {apiUrl} from "@/app/api/config";
+import {Poppins} from "next/font/google";
 
 const HeaderText = Poppins({ subsets: ["latin"], weight: "600" });
 
