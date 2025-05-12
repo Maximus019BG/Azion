b928eafa58092a6d770014c5d7e3e7c332a6e951
Maximus019BG <maxralev019@gmail.com>
2025-05-12 20:28:25 +0300

    fix: task showing on dashboard and the number of task showing on dashboard

--- server/Azion/src/main/java/com/azion/Azion/Services/TasksService.java
+++ server/Azion/src/main/java/com/azion/Azion/Services/TasksService.java
@@ -1,9 +1,9 @@
 package com.azion.Azion.Services;
 
-import com.azion.Azion.Models.*;
 import com.azion.Azion.Models.DTO.RoleDTO;
 import com.azion.Azion.Models.DTO.TasksDTO;
 import com.azion.Azion.Models.DTO.UserDTO;
+import com.azion.Azion.Models.*;
 import com.azion.Azion.Repositories.FileRepo;
 import com.azion.Azion.Repositories.OrgRepository;
 import com.azion.Azion.Repositories.RoleRepository;
@@ -30,6 +30,7 @@ public class TasksService {
     //VirusTotal config
     private static final String API_KEY = System.getProperty("virusTotalApiKey");
     private static final String VIRUSTOTAL_URL = "https://www.virustotal.com/api/v3/files";
+    private static final int TASK_SIZE = 7;
     
     private final TasksRepository tasksRepository;
     private final FileRepo fileRepo;
@@ -170,12 +171,14 @@ public class TasksService {
                 .comparing(Task::getDate) //!Compare by date
                 .thenComparing(Comparator.comparing(Task::getPriority).reversed()) //!Compare by priority
         );
+        
         List<TasksDTO> topProjects = new ArrayList<>();
         
-        if (projects.size() >= 3) {
-            topProjects.add(convertToDTO(projects.get(0)));
-            topProjects.add(convertToDTO(projects.get(1)));
-            topProjects.add(convertToDTO(projects.get(2)));
+        if (projects.size() >= TASK_SIZE) {
+            for (int index = 0; index < TASK_SIZE; index++) {
+                topProjects.add(convertToDTO(projects.get(index)));
+            }
+            
         } else {
             for (Task task : projects) {
                 topProjects.add(convertToDTO(task));
